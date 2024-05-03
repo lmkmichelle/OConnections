9f60a9a74fc851913edad201b4c67a8dda70844d
Michelle Mercer <yuming.michelle@gmail.com>
2024-05-03 13:39:33 -0400

    fixing merge

--- connections/bin/main.ml
+++ connections/bin/main.ml
@@ -8,8 +8,8 @@ let green = make_category_list "green.txt" "green"
 let blue = make_category_list "blue.txt" "blue"
 let purple = make_category_list "purple.txt" "purple"
 let size = List.length yellow
-(** Testing list of four categories, and random number used to decide which
-    categories from them are picked for a given time*)
+(* Testing list of four categories, and random number used to decide which
+    categories from them are picked for a given time *)
 let x = Random.int (size) 
 let const = [
   (List.nth yellow x);
@@ -31,16 +31,24 @@ let rec convert_to_int_list acc = function
 | hd :: tl ->
     let num = int_of_string hd in
     convert_to_int_list (num :: acc) tl
-(** Main game function.*)
-let game () = 
-  let words_array = Array.make 16 (Word.make "" "") in
-  (* Store all elements from all four categories into one [word array] *)
-  for i = 0 to (List.length const) - 1 do
-    for j = 0 to Array.length ((List.nth const i).items) - 1 do
-     words_array.(4 * i + j) <- (List.nth const i).items.(j)
-    done
-  done;
-  let () = shuffle words_array in
+
+(* Helper function to update guessed words for a category *)
+let array_eliminate (l1 : Word.t array) (l2 : Word.t array) =
+  let l2_list = Array.to_list l2 in
+  let l1_list = Array.to_list l1 in
+  let l2_filtered = (List.filter (fun word -> (word.word <> "empty")) l2_list) in
+  (* let () = List.iter (fun word -> Printf.printf "%s " word.word) l2_filtered in *)
+  (Array.of_list (List.filter (fun x -> not (List.mem x l2_filtered)) l1_list))
+
+(* Helper function to update words_array with correctly guessed words *)
+let update_words_array (words_array : Word.t array) (guessed_word : Word.t array) =
+  let guessed_list = Array.to_list guessed_word in
+  let guessed_filtered = Array.of_list ((List.filter (fun word -> word.word <> "empty")) guessed_list) in
+  let remaining = array_eliminate words_array guessed_word in
+  Array.append guessed_filtered remaining
+
+
+let rec game num words_array (guessed_words : Word.t array) =
   (* Prints out the words in a stylized 4x4 grid.*)
   for words = 0 to (Array.length words_array - 1) do 
     if (words mod 4 = 0 && words <> 0) then 
@@ -72,7 +80,6 @@ let game () =
     if word1_category = word2_category && 
       word1_category = word3_category && 
       word1_category = word4_category then
-      (* let word_list = [word1.word; word2.word; word3.word; word4.word] in *)
       let () = print_endline "Correct!" in
       (* Update guessed words *)
       let () = guessed_words.(16 - num) <- word1 in
@@ -90,11 +97,18 @@ let game () =
       word1_category = word4_category) || (word1_category = word3_category &&
       word1_category = word4_category) || (word2_category = word3_category &&
       word2_category = word4_category)
-      then print_endline "One away!"
+      then 
+      let three_category = 
+        if word1_category = word2_category then word1_category
+        else if word1_category = word3_category then word1_category
+        else if word1_category = word4_category then word1_category
+        else word2_category 
+      in 
+      let category_tried = List.find (fun x -> x.name = three_category) const in 
+      let hint = category_tried.hint in 
+      print_endline ("One Away! Hint: " ^ hint)
   else
-    print_endline "Nope!" 
-else
-  print_endline ("Enter 4 numbers for 4 words.")
+    print_endline ("Enter 4 numbers for 4 words.")
 
 let rec main_loop words_array =
   game (Array.length words_array) words_array guessed_words;
